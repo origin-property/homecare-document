a8d94855fe0d1753e1f5d8994a8a492985868350
Yossarun Khanthakamon <testonohm@gmail.com>
2024-04-02 10:30:24 +0700

    update query

--- magidoc.mjs
+++ magidoc.mjs
@@ -1,5 +1,5 @@
-import path from "path";
-import { fileURLToPath } from "url";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -8,31 +8,29 @@ export default {
   introspection: {
     // type: 'url',
     // url: 'https://qaapi.myorigin.net/graphql',
-<<<<<<< HEAD
     // headers: {
     //   Authorization: 'Bearer tmS3Cp69kpwEAB0kSIpx3GtCF496doFN',
     // },
     type: 'sdl',
-    // paths: ['./schemas/**/*.graphql'],
     paths: [path.join(__dirname, '/schemas/**/*.graphql')],
   },
   website: {
     template: 'carbon-multi-page',
-    // staticAssets: relativePath('./assets'),
     staticAssets: path.join(__dirname, '/assets'),
-=======
-    type: "sdl",
-    // paths: ['./schemas/**/*.graphql'],
-    paths: [relativePath("./schemas/**/*.graphql")],
-  },
-  website: {
-    template: "carbon-multi-page",
-    staticAssets: relativePath("./assets"),
->>>>>>> a54dfa086917b1f16a787e08760d93788d8eb542
     options: {
-      // You need to specify the base path of your github pages root
-      // Example: `/magidoc`
       siteRoot: '/demo-magidoc',
     },
+  },
+  dev: {
+    /**
+     * Globs, file paths or directories to watch for and hot-reload on change.
+     * By default, Magidoc will reload the website on change of any static assets or the magidoc.mjs file.
+     *
+     * @see https://github.com/magidoc-org/magidoc/blob/main/docs/magidoc.mjs for an example.
+     */
+    watch: [
+      path.join(__dirname, '/docs'),
+      path.join(__dirname, '/schemas/**/*.graphql'),
+    ],
   },
 };
