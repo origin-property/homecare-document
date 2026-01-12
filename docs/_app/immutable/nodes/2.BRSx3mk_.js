518879fc4f1692d7748cc9a7b053b659654fca92
UN <rattamnoon.kir@gmail.com>
2026-01-12 11:33:31 +0700

    feat: SD-2533 update docs primo plus new origin homecare
    Signed-off-by: UN <rattamnoon.kir@gmail.com>

---
+++ docs/_app/immutable/nodes/2.BRSx3mk_.js
@@ -0,0 +1,1 @@
+import{r as e}from"../chunks/C2pzervQ.js";import{h as o}from"../chunks/Da2W2RGS.js";function r(){throw e(301,o)}const l=Object.freeze(Object.defineProperty({__proto__:null,load:r},Symbol.toStringTag,{value:"Module"}));export{l as universal};
